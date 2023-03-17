82eb66a2c46c21d3abfc32e4a89c7a0073e566fe
cuteNK <zzum0701@naver.com>
2023-03-17 10:02:47 +0900

    module exports 방법

--- week2/moduleEx/moduleText.js
+++ week2/moduleEx/moduleText.js
@@ -1,9 +1,20 @@
 const text_odd = "홀";
 const text_even = "짝";
 
-console.log("1 : ");
+console.log("1 : ", exports === module.exports)
+console.log("2 : ", exports, module.exports)
 
 module.exports = {
     text_odd,   // text_odd : text_odd
     text_even,
 }
+
+// 같은 코드
+// exports.text_odd = text_odd;
+// exports.text_even = text_even;
+
+// 얜 안됨
+// exports = {
+//     text_odd,   // text_odd : text_odd
+//     text_even,
+// }
