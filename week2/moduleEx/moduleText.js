387d4178d312accbb241fb94ac786e4e29050fea
cuteNK <zzum0701@naver.com>
2023-03-17 09:26:24 +0900

    module study

---
+++ week2/moduleEx/moduleText.js
@@ -0,0 +1,9 @@
+const text_odd = "홀";
+const text_even = "짝";
+
+console.log("1 : ");
+
+module.exports = {
+    text_odd,   // text_odd : text_odd
+    text_even,
+}
---
+++ week2/moduleEx/useModule2.js
@@ -0,0 +1,7 @@
+const [check_str_OE, check_num, text_odd, text_even] = require('./useModule.js');
+// 배열이기 때문에 이름 변경 가능
+
+console.log("useModule2 : ", check_str_OE('hello'));
+console.log("useModule2 : ", check_num(5));
+console.log("useModule2 : ", text_odd);
+console.log("useModule2 : ", text_even);
