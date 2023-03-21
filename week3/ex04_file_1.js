d996a2e4b1e379a81b5983266c6a2f9a8f293950
cuteNK <zzum0701@naver.com>
2023-03-22 08:56:01 +0900

    파일쓰기

--- week3/ex04_file_1.js
+++ week3/ex04_file_1.js
@@ -7,4 +7,12 @@ fs.readFile("./readText.txt", (err, data) => {
     }
     console.log(data);
     console.log(data.toString());
+    readData = data.toString();
 });
+
+fs.writeFile("./writeText.txt", ("읽어온 데이터 : "+readData),(err)=>{
+    if(err){
+        throw err;
+    }
+    console.log("파일쓰기 완료");
+})
