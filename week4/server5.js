e0e2ff4d99da7638b0c6c3647f474c82d5df632b
cuteNK <zzum0701@naver.com>
2023-03-31 09:39:12 +0900

    폴더 안에 있는 파일 읽기

---
+++ week4/server5.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs').promises;
+
+const path = require('path');
+const url = require('url');
+
+const server = http.createServer(async (req, res)=> {
+    try {
+        console.log("URL부분 : ", req.url);
+
+        if(req.url == '/favicon.ico') {
+            res.writeHead(404);
+            res.end();
+        }
+
+        // 지정된 폴더의 파일 리스트를 읽어와 본문안에 넣기
+        const menuFolder = path.join(__dirname,'./textFile');
+        console.log("내가 읽고 싶은 폴더 : ", menuFolder);
+        const fileList = fs.readdir(menuFolder);    // readdir을 활용해서 해당 폴더의 내용을 가져옴. 프로미스 형태 (프로미스가 아니라면 콜백함수 쓰면 됨)
+        // 요소 만들기
+        let fileListText = '<ul>'
+        fileList.then((file_list)=>{
+            let ii = 0;
+            console.log("fileList", file_list)
+            while (ii < file_list.length) {
+                let dataData = file_list[ii].replace("menu_","").replace(".txt","");
+                fileListText += `<li><a href="/?date=${dataData}>`
+
+                ii+=1;
+            }
+        })
+
+        res.writeHead(200, {'Content-type':'text/plain; charset=utf-8'});
+        res.end("성공");
+
+        
+    } catch(err) {
+        console.err(err)
+        res.writeHead(500, {'Content-type':'text/plain; charset=utf-8'});
+        res.end(err,messsge);
+    }
+});
+
+server.listen(8089);
+server.on('listening',()=>{
+    console.log("8089번 포트에서 서버 대기중입니다.");
+});
