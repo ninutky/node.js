6b726614748d3483ecbfbcff20f9a3d47f7c3852
cuteNK <zzum0701@naver.com>
2023-05-12 08:45:23 +0900

    익스프레슴에 미들웨어 만들어 설정 (수정)

--- ExpressExample/app2.js
+++ ExpressExample/app2.js
@@ -1,6 +1,6 @@
-// // Express 기본 모듈 불러오기
-// var express = require('express');
-// var http = require('http');
+// Express 기본 모듈 불러오기
+var express = require('express');
+var http = require('http');
 
 // 익스프레스 객체 생성
 var app = express();
@@ -11,4 +11,12 @@ app.use(function(req, res, next) {
 
     res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
     res.end('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+});
+
+// 기본 포트를 app 객체에 속성으로 결정
+app.set('port', process.env.PORT || 3000);
+
+// Express 서버 시작
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('익스프레스 서버를 시작했습니다. : '+app.get('port'));
 });
