efdf4d2d14866338a5edc77aa8af1ea63139330f
cuteNK <zzum0701@naver.com>
2023-05-12 08:54:03 +0900

    여러 개의 미들웨어 사용 (use() 메소드 사용)

--- ExpressExample/app2.js
+++ ExpressExample/app2.js
@@ -9,8 +9,14 @@ var app = express();
 app.use(function(req, res, next) {
     console.log('첫번째 미들웨어에서 요청을 처리함.');
 
+    req.user = 'sunny';
+    next();
+});
+
+app.use('/', function(req, res, next) {
+    console.log('두 번째 미들웨어에서 요청을 처리함.');
     res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-    res.end('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+    res.end('<h1>Express 서버에서' + req.user + '가 응답한 결과입니다.</h1>');
 });
 
 // 기본 포트를 app 객체에 속성으로 결정
